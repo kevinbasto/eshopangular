9999d0cd24583fe6e5f83e55f36266361b2c7c37
Kevin Basto <neverknowpeople@hotmail.com>
2021-04-24 12:37:03 -0500

    limpieza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Hola a todos, como están?';
-}
+export class AppComponent { }
